99dbf132ab0a19e35a55948576c30e70012d42b7
HyojaeKwon-GIST20 <khj010909@gm.gist.ac.kr>
2021-08-28 14:18:01 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extents: [
+    // vue
+    'plugin:vue/vue3-strongly-recommended',
+    // js
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    
+  }
+}
